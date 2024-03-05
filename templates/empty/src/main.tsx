05176518172f203bc5590b3996b6ec6fccf47a21
Teddy Sears <teddy.sears@dynatrace.com>
2024-03-05 19:44:37 +0100

    Update react type libraries for empty template

--- templates/empty/src/main.tsx
+++ templates/empty/src/main.tsx
@@ -4,7 +4,7 @@ import { AppRoot } from "@dynatrace/strato-components-preview";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./app/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <AppRoot>
