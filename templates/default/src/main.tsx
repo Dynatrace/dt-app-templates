6fdc9f2d662b33e6e3e0ba865b61888ecd2730bb
Teddy Sears <teddy.sears@dynatrace.com>
2024-03-05 19:42:45 +0100

    Updating react type libraries

--- templates/default/src/main.tsx
+++ templates/default/src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./app/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <AppRoot>
