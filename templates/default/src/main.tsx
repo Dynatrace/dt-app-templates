6b0a7afcd3b3cb4499f4b7da7f5942c5509f366f
Teddy Sears <teddy.sears@dynatrace.com>
2024-04-11 11:48:01 +0200

    Remove react strict mode

--- templates/default/src/main.tsx
+++ templates/default/src/main.tsx
@@ -1,16 +1,14 @@
-import { AppRoot } from "@dynatrace/strato-components-preview";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AppRoot } from "@dynatrace/strato-components-preview";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./app/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-  <React.StrictMode>
-    <AppRoot>
-      <BrowserRouter basename="ui">
-        <App />
-      </BrowserRouter>
-    </AppRoot>
-  </React.StrictMode>
+  <AppRoot>
+    <BrowserRouter basename="ui">
+      <App />
+    </BrowserRouter>
+  </AppRoot>
 );
--- templates/empty/src/main.tsx
+++ templates/empty/src/main.tsx
@@ -6,11 +6,9 @@ import { App } from "./app/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-  <React.StrictMode>
-    <AppRoot>
-      <BrowserRouter basename="ui">
-        <App />
-      </BrowserRouter>
-    </AppRoot>
-  </React.StrictMode>
+  <AppRoot>
+    <BrowserRouter basename="ui">
+      <App />
+    </BrowserRouter>
+  </AppRoot>
 );
