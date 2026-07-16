d24ce5739a0b2d874edecce620a5e460f009555b
SmonPrDT <simon.primetzhofer@dynatrace.com>
2026-07-16 13:45:50 +0200

    version updates and fix deprecations (#91)

--- templates/default/ui/app/App.tsx
+++ templates/default/ui/app/App.tsx
@@ -1,4 +1,4 @@
-import { Page } from "@dynatrace/strato-components/layouts";
+import { PageLayout } from "@dynatrace/strato-components/layouts";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Data } from "./pages/Data";
@@ -7,16 +7,16 @@ import { Home } from "./pages/Home";
 
 export const App = () => {
   return (
-    <Page>
-      <Page.Header>
+    <PageLayout>
+      <PageLayout.Header>
         <Header />
-      </Page.Header>
-      <Page.Main>
+      </PageLayout.Header>
+      <PageLayout.Content>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/data" element={<Data />} />
         </Routes>
-      </Page.Main>
-    </Page>
+      </PageLayout.Content>
+    </PageLayout>
   );
 };
